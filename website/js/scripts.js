59ac024b9f03b188f05140d658d7b50bc4e2c524
Joey Nutinsky <joey@ruvna.com>
2018-02-12 23:26:58 -0500

    Improved SVG animation

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -4,4 +4,28 @@ $(document).ready(function(){
     gallery_theme: "tiles",
     tiles_type:"justified"
   }); 
+  
+  $('#made-for-music').bind('inview', function(event, visible) {
+    if(visible == true) {
+      
+    } else {
+      $("#svg-holder").addClass("show-item");
+      console.log("HERE");
+    }
+  });
+  
+  $(window).scroll(function() {
+    console.log($(window).scrollTop());
+    var topDivHeight = $("#made-for-music").height();
+    var viewPortSize = $(window).height();
+
+    var triggerAt = 1200;
+    var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
+    console.log(triggerHeight);
+    if ($(window).scrollTop() >= triggerHeight) {
+        console.log("SHOWING");
+        $('#svg-holder').addClass("show-item");
+        $(this).off("scroll");
+    }
+  })
 }); 
